bad45aa8cd51e5fc06efd46b02928d713b38d66b
Nathalie <nathalievzm@hotmail.com>
2021-07-22 15:09:32 -0400

    prueba imagenes

--- app.py
+++ app.py
@@ -201,11 +201,16 @@ def image_create():
 
 
 # * APP.RUN()
+csrf.init_app(app)
 db.init_app(app)
-if __name__ == '__main__':
-    csrf.init_app(app)
-    # db.init_app(app)
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
-    app.run()
+app.run()
+# if __name__ == '__main__':
+#     csrf.init_app(app)
+#     db.init_app(app)
+#     with app.app_context():
+#         db.create_all()
+
+#     app.run()
