afaba3c75ecf81d71c6b36855d73af1e2072820d
Nathalie <nathalievzm@hotmail.com>
2021-07-23 22:45:19 -0400

    verificaciones hechas

--- static/js/input_file.js
+++ static/js/input_file.js
@@ -1,13 +1,31 @@
 //input_file.js
 
 let file = document.getElementById('file')
+let add = document.getElementById('add')
 
-if(file){
+if(file && add){
     const showFile = (e) => {
         console.log(file.files[0].name)
         let label = document.querySelector('.inputbox label')
-        label.innerHTML = file.files[0].name
+        if(file.files[0].size > 3145728){
+            label.innerHTML = 'File is too big'
+            file.value = ''
+        }
+        else {
+            label.innerHTML = file.files[0].name
+        }
     }
     
     file.onchange = showFile
+
+    const sendFile = (e) => {
+        console.log(e.target.id)
+
+        if(file.value == ''){
+            let label = document.querySelector('.inputbox label')
+            label.innerHTML = 'Choose a file'
+        }
+    }
+
+    add.onclick = sendFile
 }
